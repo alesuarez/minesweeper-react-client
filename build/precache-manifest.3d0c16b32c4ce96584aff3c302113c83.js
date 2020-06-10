696e30e19a52cdc245e993ae1ef7b060324c228f
Sebastian Alejandro Suarez <sas.tuc@gmail.com>
2020-06-10 19:43:55 -0300

    serve

---
+++ build/precache-manifest.3d0c16b32c4ce96584aff3c302113c83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca00ff0aec051069da864731ba1be2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc0707b7dbfd42cd80e9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f2c773c2c8eaa6a4720",
+    "url": "/static/js/2.a5491260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5491260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0707b7dbfd42cd80e9",
+    "url": "/static/js/main.834f5021.chunk.js"
+  },
+  {
+    "revision": "1758ac9401634839807e",
+    "url": "/static/js/runtime-main.0f9a9671.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
